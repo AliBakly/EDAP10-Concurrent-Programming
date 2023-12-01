24fb77cac8f6e2eb33b716e0f29e24db7ac19e93
Ali7538 <anbakly@gmail.com>
2023-12-01 17:56:56 +0100

    Updated lift to run infinitely

--- lift/src/PassengerThread.java
+++ lift/src/PassengerThread.java
@@ -3,37 +3,40 @@ import lift.Passenger;
 
 public class PassengerThread extends Thread{
 	private PassengerLiftMonitor monitor;
-	private final Passenger pass;
-
+	private Passenger pass;
+	private final LiftView view;
 	public PassengerThread(PassengerLiftMonitor monitor, LiftView view) {
 		this.monitor = monitor;
+		this.view = view;
 		pass = view.createPassenger();
 	}
 	 
 	@Override
 	public void run() {
-		pass.begin();
-		int fromFloor = pass.getStartFloor();
-        int toFloor = pass.getDestinationFloor();
-        
-        try {
-        	monitor.addToEnter(fromFloor);
-			monitor.waitForLift(fromFloor, pass);
-			pass.enterLift();
-			monitor.notifyEntry(fromFloor);
-			
-			monitor.addToExit(toFloor);
-			monitor.waitForDestination(toFloor, pass);
-			pass.exitLift();
-			monitor.notifyExit(toFloor);
-			
-			//FIX INTERRUPTED EXC
-        } catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		while(true) {
+			pass.begin();
+			int fromFloor = pass.getStartFloor();
+	        int toFloor = pass.getDestinationFloor();
+	        
+	        try {
+	        	monitor.addToEnter(fromFloor);
+				monitor.waitForLift(fromFloor, pass);
+				pass.enterLift();
+				monitor.notifyEntry(fromFloor);
+				
+				monitor.addToExit(toFloor);
+				monitor.waitForDestination(toFloor, pass);
+				pass.exitLift();
+				monitor.notifyExit(toFloor);
+				
+				//FIX INTERRUPTED EXC
+	        } catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	        
+	        pass.end();
+	        pass = view.createPassenger();
 		}
-        
-        pass.end();
-        
 	}
 }
